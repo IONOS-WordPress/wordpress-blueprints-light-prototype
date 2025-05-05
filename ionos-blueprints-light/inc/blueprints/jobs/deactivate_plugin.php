0e8197c0e051fa39df0ce0cab573eeedeed44e15
Lars Gersmann <lars.gersmann@ionos.com>
2025-05-05 13:29:23 +0200

    feat: add deactivate_plugin job and schema with silent option

---
+++ ionos-blueprints-light/inc/blueprints/jobs/deactivate_plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ionos_blueprints_light\ionos_blueprints_light\blueprints;
+
+use const ionos_blueprints_light\ionos_blueprints_light\blueprints\CRON_JOB_HOOK;
+
+if ( ! defined( 'ABSPATH' ) ) {
+  die();
+}
+
+\add_filter( CRON_JOB_HOOK . '_deactivate_plugin', function(array $payload) : array {
+  $args = $payload['args'];
+  
+  $slug = $args['slug'];
+  $silent = $args['silent'] ?? false;
+
+  \deactivate_plugins($slug, $silent);
+   
+  return [
+    'success' => true,
+  ];
+});
---
+++ ionos-blueprints-light/tests/DeactivatePluginJobTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ionos_blueprints_light\ionos_blueprints_light\phpunit;
+
+use const ionos_blueprints_light\ionos_blueprints_light\blueprints\CRON_JOB_HOOK;
+use const ionos_blueprints_light\ionos_blueprints_light\blueprints\OPTION_JOBS_DONE;
+use const ionos_blueprints_light\ionos_blueprints_light\blueprints\OPTION_JOBS_SCHEDULED;
+use const ionos_blueprints_light\ionos_blueprints_light\SLUG;
+
+require_once __DIR__ . '/../blueprints-light.php';
+
+class DeactivatePluginJobTest extends \WP_UnitTestCase {
+
+  public function setUp(): void {
+    parent::set_up();
+
+    \activate_plugin( SLUG );
+  }
+
+  public function tearDown(): void {
+    parent::tear_down();
+
+    \deactivate_plugins(SLUG);
+  }
+
+  function test_deactivate_job() {
+    \update_option(OPTION_JOBS_SCHEDULED, [
+      [
+        'id' => $uuids[]=\wp_generate_uuid4(),
+        'type' => 'deactivate_plugin',
+        'args' => [
+          'slug'=> SLUG
+        ]
+      ],
+    ]);
+
+    // trigger init to initialize our plugin 
+    \do_action( 'init' );
+    // verify our plugin initialized its cron job
+    $this->assertTrue( \wp_next_scheduled(CRON_JOB_HOOK) !== false, 'cron job should be cleaned up' );
+
+    // execute cron job the first time
+    \do_action(CRON_JOB_HOOK);
+
+    // verify our plugin is no more active
+    $this->assertFalse( \is_plugin_active(SLUG), 'plugin should be deactivated' );
+
+    // verify the plugin deactivation hook was properly called
+    $this->assertFalse( \wp_next_scheduled(CRON_JOB_HOOK), 'cron job should be cleaned up' );
+  }
+
+  function test_deactivate_job_silent() {
+    \update_option(OPTION_JOBS_SCHEDULED, [
+      [
+        'id' => $uuids[]=\wp_generate_uuid4(),
+        'type' => 'deactivate_plugin',
+        'args' => [
+          'slug'=> SLUG,
+          'silent' => true,
+        ]
+      ],
+    ]);
+
+    // trigger init to initialize our plugin 
+    \do_action( 'init' );
+    // verify our plugin initialized its cron job
+    $this->assertTrue( \wp_next_scheduled(CRON_JOB_HOOK) !== false, 'cron job should be cleaned up' );
+
+    // execute cron job the first time
+    \do_action(CRON_JOB_HOOK);
+
+    // verify our plugin is no more active
+    $this->assertFalse( \is_plugin_active(SLUG), 'plugin should be deactivated' );
+
+    // verify the plugin deactivation hook was not called
+    $this->assertTrue( \wp_next_scheduled(CRON_JOB_HOOK)!==false, 'cron job should not be be cleaned up' );
+  }
+}
